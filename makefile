9c55e67311b83d6687bac8dce9e948198694d136
sokolcom <suget169@gmail.com>
2021-11-04 20:56:50 +0300

    [ADD]: ecdsa

--- makefile
+++ makefile
@@ -5,8 +5,12 @@ CXXFLAGS = -std=c++11 #-Wall -Werror
 SRC_UINT256 = $(wildcard uint256_t/*.cpp)
 OBJ_UINT256 = $(patsubst %.cpp, %.o, $(wildcard $(SRC_UINT256)))
 
+SRC_SHA256 = $(wildcard sha256/src/*.cpp)
+OBJ_SHA256 = $(patsubst %.cpp, %.o, $(wildcard $(SRC_SHA256)))
+
 SRC_CXX = $(wildcard src/*.cpp)
 OBJ_SRC = $(patsubst %.cpp, %.o, $(wildcard $(SRC_CXX)))
+
 
 
 app.out: $(OBJ_UINT256) $(OBJ_SRC) 
@@ -14,14 +18,8 @@ app.out: $(OBJ_UINT256) $(OBJ_SRC)
 
 #$(OBJ_SRC):		CXXFLAGS += -lcrypto
 $(OBJ_UINT256): CXXFLAGS += -fPIC
+$(OBJ_SHA256):	CXXFLAGS += -fPIC
 
-
-split: app.out
-	./$^ split
-
-
-recover: app.out
-	./$^ recover
 
 
 clean:
--- sha256/src/main.cpp
+++ sha256/src/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
-#include "SHA256.h"
+#include <utility>
+// #include "SHA256.h"
 
 int main(int argc, char ** argv) {
 
-	for(int i = 1 ; i < argc ; i++) {
-		SHA256 sha;
-		sha.update(argv[i]);
-		uint8_t * digest = sha.digest();
+	int a = 0;
+	int b = 2;
+	std::pair<int*, int*> c = std::make_pair(&a, &b);
 
-		std::cout << SHA256::toString(digest) << std::endl;
-
-		delete[] digest;
+	if (!c.first) {
+		std::cout << "YES!\n";
+	} else {
+		std::cout << "NO!\n";
 	}
+
+	// for(int i = 1 ; i < argc ; i++) {
+	// 	SHA256 sha;
+	// 	sha.update(argv[i]);
+	// 	uint8_t * digest = sha.digest();
+
+	// 	std::cout << SHA256::toString(digest) << std::endl;
+
+	// 	delete[] digest;
+	// }
 
 	return EXIT_SUCCESS;
 }
