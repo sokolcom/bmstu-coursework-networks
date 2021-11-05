df1ceed5fd85e56a00efab6914b900192b780925
Vlad <vkrivozubov@list.ru>
2021-11-05 14:31:49 +0300

    [new] full protocol support

--- car.cpp
+++ car.cpp
@@ -14,6 +14,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <string>
+#include <nlohmann/json.hpp>
+#include <set>
 
 #define PORT 8888
 #define SERVER_IP "127.0.0.1"
@@ -22,10 +24,62 @@
 
 using namespace std;
 
+std::set<std::string> user_tokens = { "666" };
+std::set<std::string> user_sessions = {};
+
 void perror_and_exit(std::string err_msg, size_t exit_code)
 {
 	perror(err_msg.c_str());
 	exit(exit_code);
+}
+
+int accept_connection(int listener, struct sockaddr_in client_addr) {
+	int sock;
+	socklen_t cli_addr_size = sizeof(client_addr);
+	std::cout << "here1" << std::endl;
+	sock = accept(listener, (struct sockaddr*) &client_addr, &cli_addr_size);
+	if(sock < 0) {
+		std::cout << "here4334" << std::endl;
+		perror_and_exit("accept()", 3);
+	}
+	std::cout << "here2" << std::endl;
+	return sock;
+}
+
+bool is_user_authorized(std::string auth_token) {
+	auto search = user_tokens.find(auth_token);
+
+	if (search != user_tokens.end()) {
+		return true; 
+	}
+
+	return false;
+}
+
+bool is_session_exist(std::string auth_token) {
+	auto search = user_sessions.find(auth_token);
+
+	if (search != user_sessions.end()) {
+		std::cout << "session exists" << std::endl;
+		return true;
+	}
+
+	std::cout << "session not exists" << std::endl;
+	return false;
+}
+
+bool is_chapter_correct(std::string auth_token, std::string chapter) {
+	bool session_exists = is_session_exist(auth_token);
+	if (chapter == "handshake" && !session_exists) {
+		std::cout << "handhake chapter correct" << std::endl;
+		return true;
+	} else if (chapter == "response" && session_exists) {
+		std::cout << "response chapter correct" << std::endl;
+		return true;
+	}
+	
+	std::cout << "chapter failed" << std::endl;
+	return false;
 }
 
 int main()
@@ -45,48 +99,94 @@ int main()
 	}
 
 	listen(listener, 10);
+	printf("Server is listening on %s:%d...\n", SERVER_IP, PORT);
+	
+	int sock = accept_connection(listener, client_addr);
+	bool is_executing = true;
 
-		printf("Server is listening on %s:%d...\n", SERVER_IP, PORT);
-	//ThreadPool tp;
-	while(1)
+	while(is_executing)
 	{
-
-		int sock;
 		int bytes_read;
 		char *buf = (char*)malloc(MSG_LEN);
-		socklen_t cli_addr_size = sizeof(client_addr);
-		std::cout << "here1" << std::endl;
-		sock = accept(listener, (struct sockaddr*) &client_addr, &cli_addr_size);
-		if(sock < 0) {
-			std::cout << "here4334" << std::endl;
-			perror_and_exit("accept()", 3);
-		}
-		std::cout << "here2" << std::endl;
 		bytes_read = recv(sock, buf, MSG_LEN, 0);
 		if (bytes_read < 0)
 		{
-			std::cout << "here3" << std::endl;
+			std::cout << "here3 " << bytes_read << std::endl;
 			printf("Recv failed");
 			close(sock);
-			continue ;
+			is_executing = false;
+			continue;
 		}
 		if (bytes_read == 0)
 		{
 			std::cout << "here4" << std::endl;
 			puts("Client disconnected upexpectedly.");
 			close(sock);
+			accept_connection(listener, client_addr);
 			continue ;
 		}
 		buf[bytes_read] = '\0';
 		cout << "buf" << buf << "\n";
 		char tst[MSG_LEN];
 		strcpy(tst, buf);
-		std::cout << "here5" << std::endl;
-		std::string s = "{ \"random_number\": \"fook\", \"hash\": \"s123435422423423\"}";
-		send(sock, s.c_str(), s.size(), 0);
-		//tp.queueWork(sock, tst);
-		std::cout << "here6" << std::endl;
-		free(buf);
+		std::cout << "here5 " << buf <<std::endl;
+		std::string message = std::string(buf);
+		nlohmann::json js = nlohmann::json::parse(message);
+		std::string user_token = js.at("auth_token");
+		std::string chapter = js.at("chapter");
+		std::cout << "car - " << user_token << " " << chapter << std::endl;
+		if (!is_user_authorized(user_token)) {
+			puts("Not authorized");
+			close(sock);
+			accept_connection(listener, client_addr);
+			continue;
+		}
+
+		if (!is_chapter_correct(user_token, chapter)) {
+			puts("Invalid chapter");
+			std::cout << chapter << std::endl;
+			close(sock);
+			accept_connection(listener, client_addr);
+			continue;
+		}
+
+		if (chapter == "handshake") {
+			std::cout << "handshake handling" << std::endl;
+			user_sessions.insert(user_token);
+			std::string nonce = "coco-jambo";
+			std::string hash = "s123435422423423";
+			std::string s = "{ \"nonce\": \"" + nonce + "\", \"hash\": \"" + hash + "\"}";
+			send(sock, s.c_str(), s.size(), 0);
+		} else if (chapter == "response") {
+			std::cout << "response handling" << std::endl;
+			std::cout << "car opened" << std::endl;
+			for(auto it = user_sessions.begin(); it != user_sessions.end(); ) {
+				if(*it == user_token) {
+					it = user_sessions.erase(it);
+					break;
+				} else {
+					++it;
+				}
+    		}
+
+			std::string nonce = js.at("nonce");
+			std::string hash = js.at("hash");
+			std::string success = "true";
+			std::string s = "{ \"success\": " + success + " }";
+			std::cout << s << std::endl;
+			send(sock, s.c_str(), s.size(), 0);
+
+			puts("Succesfully finished");
+			close(sock);
+			accept_connection(listener, client_addr);
+			continue;
+
+		} else {
+			puts("Invalid chapter");
+			close(sock);
+			accept_connection(listener, client_addr);
+			continue;
+		}
 	}
 	close(listener);
 	return 0;
--- src/person.cpp
+++ src/person.cpp
@@ -21,60 +21,10 @@
 
 using namespace std;
 
-void send_request() {
-	int person_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    
-    if (person_socket < 0) {
-        //return ERROR;
-    }
-
-    struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(PORT);
-	server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-	if (connect(person_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
-        //return ERROR;
-    }
-
-	//printf("enter your Name: \n");
-	//cin >> userName;
-
-	//char message[MSG_LEN];
-	//printf("enter url: \n");
-	//scanf("%s", message);
-
-	//string mes = generateGetMessage(message);
-	//const char * msg = mes.c_str();
-
-	//printf("Sending message...\n\n");
-
-	//cout << "msg " << msg << "\n";
-    char message[MSG_LEN];
-	std::string handshake_message = "{\"auth_token\": " + std::string(USER_TOKEN) + ", chapter: \"handshake\"}";
-    const char *handhake_c_string = handshake_message.c_str();
-	sendto(person_socket, handhake_c_string, strlen(handhake_c_string), 0, (struct sockaddr*) &server_addr, sizeof(server_addr));
-
-	unsigned int server_addr_len = sizeof(server_addr);
-	if (recvfrom(person_socket, message, MSG_LEN, 0, (struct sockaddr*) &server_addr, &server_addr_len) == -1) {
-        //return ERROR;
-    }
-
-    std::string handshake_response_msg = std::string(message); 
-    auto js = nlohmann::json::parse(handshake_response_msg);
-	std::cout << std::string(js.at("random_number")).c_str() << " " << js.at("hash") << std::endl;
-	std::cout << handshake_response_msg;
-    // decode handshake response
-
-	close(person_socket);
-}
-
 int main() {
     uint256_t person_private_key = uint256_t(PERSON_PRIVATE_KEY);
     pair<uint256_t, uint256_t> car_public_key = make_pair(CAR_PUBLIC_KEY_FIRST, CAR_PUBLIC_KEY_SECOND);
-	send_request();
-	send_request();
-	/*
+
     int person_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     
     if (person_socket < 0) {
@@ -91,7 +41,7 @@ int main() {
     }
 
     char message[MSG_LEN];
-	std::string handshake_message = "{\"auth_token\": " + std::string(USER_TOKEN) + ", chapter: \"handshake\"}";
+	std::string handshake_message = "{\"auth_token\": \"" + std::string(USER_TOKEN) + "\", \"chapter\": \"handshake\"}";
     const char *handhake_c_string = handshake_message.c_str();
 	sendto(person_socket, handhake_c_string, strlen(handhake_c_string), 0, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
@@ -102,31 +52,32 @@ int main() {
 
     std::string handshake_response_msg = std::string(message); 
     auto js = nlohmann::json::parse(handshake_response_msg);
-	std::cout << std::string(js.at("random_number")).c_str() << " " << js.at("hash") << std::endl;
+	std::string nonce = js.at("nonce");
+	std::string hash = js.at("hash");
+	std::cout << "person received challenge: " << nonce << " " << hash << std::endl;
 	std::cout << handshake_response_msg;
-    // decode handshake response 
 
-	close(person_socket);
-	person_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // decode handshake response
 
-	std::string response_message = "{\"auth_token\": " + std::string(USER_TOKEN) + ", chapter: \"response\"}";
-    const char *response_c_string = handshake_message.c_str();
+	char new_message[MSG_LEN];
+
+	std::string response_message = "{\"auth_token\": \"" + std::string(USER_TOKEN) + "\", \"chapter\": \"response\", \"nonce\": \"fook\", \"hash\": \"abracadabra\"}";
+    const char *response_c_string = response_message.c_str();
 	sendto(person_socket, response_c_string, strlen(response_c_string), 0, (struct sockaddr*) &server_addr, sizeof(server_addr));
-	std::cout << "first" << std::endl;
-	if (recvfrom(person_socket, message, MSG_LEN, 0, (struct sockaddr*) &server_addr, &server_addr_len) == -1) {
+	if (recvfrom(person_socket, new_message, MSG_LEN, 0, (struct sockaddr*) &server_addr, &server_addr_len) == -1) {
         return ERROR;
     }
-	std::cout << "second" << std::endl;
-	std::string response_response_msg = std::string(message);
+	std::cout <<  std::string(new_message) << std::endl;
+	std::string response_response_msg = std::string(new_message);
 	auto js_response = nlohmann::json::parse(response_response_msg);
 	std::cout << js_response << std::endl;
-	std::cout << std::string(js.at("random_number")).c_str() << " " << js.at("hash") << std::endl;
+	bool success = js_response.at("success");
+	if (success) {
+		std::cout << "car opened" << std::endl;
+	} else {
+		std::cout << "car not opened" << std::endl;
+	}
 
 	close(person_socket);
-	*/
 	return 0;
-}
-
-void handshake() {
-
 }
